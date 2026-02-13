e65631b5e873367a704018ac8cdc8eaacf3af156
CleinerFG <149598883+CleinerFG@users.noreply.github.com>
2026-02-13 17:30:22 -0300

    feat(movie): create DTOs

---
+++ src/main/java/com/mymovie/api/dto/request/MovieRequest.java
@@ -0,0 +1,14 @@
+package com.mymovie.api.dto.request;
+
+import java.time.LocalDate;
+import java.util.List;
+
+public record MovieRequest(
+        String title,
+        String description,
+        LocalDate releaseDate,
+        Double rating,
+        List<Long> categoryIds,
+        List<Long> streamingIds
+) {
+}
