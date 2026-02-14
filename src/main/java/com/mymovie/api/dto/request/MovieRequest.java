f9d59e504178401ef61390e42dfcb7bf4f1b8d71
CleinerFG <149598883+CleinerFG@users.noreply.github.com>
2026-02-14 02:23:49 -0300

    feat(movie): set DTOs releaseDate with ISO 8601

--- src/main/java/com/mymovie/api/dto/request/MovieRequest.java
+++ src/main/java/com/mymovie/api/dto/request/MovieRequest.java
@@ -1,12 +1,17 @@
 package com.mymovie.api.dto.request;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+
 import java.time.LocalDate;
 import java.util.List;
 
 public record MovieRequest(
         String title,
         String description,
+
+        @JsonFormat(pattern = "yyyy-MM-dd") // ISO 8601
         LocalDate releaseDate,
+
         Double rating,
         List<Long> categoryIds,
         List<Long> streamingIds
--- src/main/java/com/mymovie/api/dto/response/MovieResponse.java
+++ src/main/java/com/mymovie/api/dto/response/MovieResponse.java
@@ -1,5 +1,6 @@
 package com.mymovie.api.dto.response;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Builder;
 
 import java.time.LocalDate;
@@ -10,7 +11,10 @@ public record MovieResponse(
         Long id,
         String title,
         String description,
+
+        @JsonFormat(pattern = "yyyy-MM-dd") // ISO 8601
         LocalDate releaseDate,
+
         Double rating,
         List<CategoryResponse> categories,
         List<StreamingResponse> streamings
