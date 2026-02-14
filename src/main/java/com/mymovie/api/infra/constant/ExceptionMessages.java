2a80a391ded3d7ceb47644370ab882931930c0fe
CleinerFG <149598883+CleinerFG@users.noreply.github.com>
2026-02-14 03:28:58 -0300

    feat: create ExceptionMessages utility class to centralize error constants

---
+++ src/main/java/com/mymovie/api/infra/constant/ExceptionMessages.java
@@ -0,0 +1,12 @@
+package com.mymovie.api.infra.constant;
+
+import lombok.experimental.UtilityClass;
+
+@UtilityClass
+public class ExceptionMessages {
+
+    public static final String RESOURCE_NOT_FOUND = "resource.not_found";
+    public static final String MOVIE_NOT_FOUND = "movie.not_found";
+    public static final String CATEGORY_NOT_FOUND = "category.not_found";
+    public static final String STREAMING_NOT_FOUND = "streaming.not_found";
+}
