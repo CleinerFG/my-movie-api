ff80b5161aa76b28fc868df8b4c2db721171074a
CleinerFG <149598883+CleinerFG@users.noreply.github.com>
2026-02-18 17:16:20 -0300

    feat(auth): implement LoginService for JWT authentication
    This service orchestrates the authentication flow by using the AuthenticationManager to validate credentials against the AuthService. Upon successful validation, it extracts the User principal to generate a JWT via TokenService. It also implements security best practices by catching BadCredentialsException and throwing a generic domain exception to prevent user enumeration.

---
+++ src/main/java/com/mymovie/api/service/LoginService.java
@@ -0,0 +1,38 @@
+package com.mymovie.api.service;
+
+import com.mymovie.api.dto.request.LoginRequest;
+import com.mymovie.api.dto.request.LoginResponse;
+import com.mymovie.api.entity.User;
+import com.mymovie.api.infra.config.TokenService;
+import com.mymovie.api.infra.constant.ExceptionMessages;
+import com.mymovie.api.infra.exception.UsernameOrPasswordInvalidException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class LoginService {
+
+    private final AuthenticationManager authenticationManager;
+    private final TokenService tokenService;
+
+    public LoginResponse authenticate(LoginRequest dto) {
+        try {
+            var authToken = new UsernamePasswordAuthenticationToken(
+                    dto.email(), dto.password());
+            Authentication authentication = authenticationManager.authenticate(authToken);
+
+            User user = (User) authentication.getPrincipal();
+            String token = tokenService.generateToken(user);
+
+            return LoginResponse.builder().token(token).build();
+        } catch (BadCredentialsException e) {
+            throw new UsernameOrPasswordInvalidException(ExceptionMessages.USERNAME_OR_PASSWORD_INVALID);
+        }
+
+    }
+}
