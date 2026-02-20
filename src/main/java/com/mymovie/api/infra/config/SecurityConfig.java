e43b03a6f41ea3f90743dced9b4300d396f313b5
CleinerFG <149598883+CleinerFG@users.noreply.github.com>
2026-02-19 23:22:31 -0300

    feat(config): add securityFilter to the filter chain in SecurityConfig
    Ensures that the JWT validation occurs before the standard authentication process.

--- src/main/java/com/mymovie/api/infra/config/SecurityConfig.java
+++ src/main/java/com/mymovie/api/infra/config/SecurityConfig.java
@@ -14,11 +14,14 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
 @RequiredArgsConstructor
 public class SecurityConfig {
+
+    private final SecurityFilter securityFilter;
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
@@ -32,6 +35,7 @@ public class SecurityConfig {
                         .requestMatchers(HttpMethod.POST, "/api/auth/login").permitAll()
                         .anyRequest().authenticated()
                 )
+                .addFilterBefore(securityFilter, UsernamePasswordAuthenticationFilter.class)
                 .build();
     }
 
