06312c0e857a226ba1a5c17db2f33c83e9ad781f
CleinerFG <149598883+CleinerFG@users.noreply.github.com>
2026-02-20 15:46:09 -0300

    feat: add VALIDATION_FAILED message in ExceptionMessages

--- src/main/java/com/mymovie/api/infra/constant/ExceptionMessages.java
+++ src/main/java/com/mymovie/api/infra/constant/ExceptionMessages.java
@@ -6,6 +6,7 @@ import lombok.experimental.UtilityClass;
 public class ExceptionMessages {
 
     public static final String USERNAME_OR_PASSWORD_INVALID = "auth.username_or_password_invalid";
+    public static final String VALIDATION_FAILED = "validation.failed";
     public static final String RESOURCE_NOT_FOUND = "resource.not_found";
     public static final String MOVIE_NOT_FOUND = "movie.not_found";
     public static final String CATEGORY_NOT_FOUND = "category.not_found";
