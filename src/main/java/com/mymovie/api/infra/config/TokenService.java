d26318fc2e4ddc448fa2c9383419038715ca3543
CleinerFG <149598883+CleinerFG@users.noreply.github.com>
2026-02-19 23:19:13 -0300

    feat(infra): implement token validation logic in TokenService
    The validateToken method uses the HMAC256 algorithm to verify the JWT integrity.

--- src/main/java/com/mymovie/api/infra/config/TokenService.java
+++ src/main/java/com/mymovie/api/infra/config/TokenService.java
@@ -2,11 +2,14 @@ package com.mymovie.api.infra.config;
 
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.exceptions.JWTVerificationException;
+import com.auth0.jwt.interfaces.DecodedJWT;
 import com.mymovie.api.entity.User;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 import java.time.Instant;
+import java.util.Optional;
 
 @Component
 public class TokenService {
@@ -27,5 +30,24 @@ public class TokenService {
                 .withIssuedAt(Instant.now())
                 .withIssuer(issuer)
                 .sign(algorithm);
+    }
+
+    public Optional<JWTUserData> validateToken(String token) {
+        try {
+            Algorithm algorithm = Algorithm.HMAC256(secret);
+
+            DecodedJWT verify = JWT.require(algorithm)
+                    .build()
+                    .verify(token);
+
+            return Optional.of(JWTUserData
+                    .builder()
+                    .id(verify.getClaim("id").asLong())
+                    .email(verify.getSubject())
+                    .build()
+            );
+        } catch (JWTVerificationException e) {
+            return Optional.empty();
+        }
     }
 }
