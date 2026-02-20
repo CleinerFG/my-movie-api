2da9ba2d62b3caf4f331323f8972bbec89007142
CleinerFG <149598883+CleinerFG@users.noreply.github.com>
2026-02-20 17:32:54 -0300

    feat(security): permit public access to documentation endpoints

--- src/main/java/com/mymovie/api/infra/config/SecurityConfig.java
+++ src/main/java/com/mymovie/api/infra/config/SecurityConfig.java
@@ -33,6 +33,7 @@ public class SecurityConfig {
                         .dispatcherTypeMatchers(DispatcherType.ERROR).permitAll()
                         .requestMatchers(HttpMethod.POST, "/api/auth/register").permitAll()
                         .requestMatchers(HttpMethod.POST, "/api/auth/login").permitAll()
+                        .requestMatchers(HttpMethod.GET, "/api/docs/**").permitAll()
                         .anyRequest().authenticated()
                 )
                 .addFilterBefore(securityFilter, UsernamePasswordAuthenticationFilter.class)
