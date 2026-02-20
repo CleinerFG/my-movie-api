5fe91ecdcd03d0a72f00354bc05338011a995563
CleinerFG <149598883+CleinerFG@users.noreply.github.com>
2026-02-20 15:43:59 -0300

    feat: add ValidationMessages

---
+++ src/main/java/com/mymovie/api/infra/constant/ValidationMessages.java
@@ -0,0 +1,8 @@
+package com.mymovie.api.infra.constant;
+
+import lombok.experimental.UtilityClass;
+
+@UtilityClass
+public class ValidationMessages {
+    public static final String REQUIRED = "validation.required";
+}
